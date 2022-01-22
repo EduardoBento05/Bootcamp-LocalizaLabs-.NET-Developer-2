50f8ba14eae2cbc13e29b6fc18a606d4dcc5f65a
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2022-01-22 18:09:41 -0300

    Add files via upload

---
+++ #C Dio/DigitalInnovationOne/DigitalInnovationOne/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+int nvezes = 5;
+
+for(int i = 0; i < nvezes; i++)
+    Console.WriteLine($"Bem Vindo ao Curso de .NET {i}");
