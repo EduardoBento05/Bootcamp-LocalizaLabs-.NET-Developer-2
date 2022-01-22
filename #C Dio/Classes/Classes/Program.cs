de230e414c40b2bc4423d926fe841084a67779aa
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2022-01-22 19:48:48 -0300

    Add files via upload

---
+++ #C Dio/Classes/Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes.Herança;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Ponto p1 = new Ponto(10,20);
+
+Ponto p2 = new Ponto3D(10,20,30);
+
