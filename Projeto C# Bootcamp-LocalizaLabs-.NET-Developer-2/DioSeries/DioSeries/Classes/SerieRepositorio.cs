cb9ef44bbf336437d41d7d80857ced9d23b91458
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2022-02-02 14:36:46 -0300

    Add files via upload

---
+++ Projeto C# Bootcamp-LocalizaLabs-.NET-Developer-2/DioSeries/DioSeries/Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using DioSeries.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioSeries.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> lista()
+        {
+            return listaSerie;  
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
